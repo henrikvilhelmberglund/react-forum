1b879cd30dcb08ed21cb73fbdf439dddb007dda2
Ingela Jonsson <ingela.ma.jonsson@gmail.com>
2023-09-07 13:08:31 +0200

    profile info added

--- src/pages/profile/[userID]/index.jsx
+++ src/pages/profile/[userID]/index.jsx
@@ -1,3 +1,30 @@
+import { useLoaderData, useNavigation } from "react-router-dom";
+
 export default function userID() {
-  return <>This is /post/:userID</>;
+  const data = useLoaderData();
+  const navigation = useNavigation();
+
+  if (navigation.state === "loading") {
+    return <div>
+      <img src="https://i.gifer.com/ZKZg.gif" alt="loading gif" />
+    </div>
+  }
+
+  return (
+    <div>
+      <p>Username: {data.username}</p>
+      <p>Email: {data.email}</p>
+      <p>City: {data.address.city}</p>
+      <p>Company: {data.company.name}</p>
+    </div>
+  )
 }
+
+export const Loader = async ({ params }) => {
+  let response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.userID}`
+  );
+  let data = await response.json();
+  console.log(data);
+  return data;
+};
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,3 +1,3 @@
 export default function Profile() {
-  return <>This is /profile</>;
+  return <></>;
 }
