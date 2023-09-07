31d172edac9830acfdacf29c7c9247c16c4ac780
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-07 11:25:03 +0200

    feat: :sparkles: add link to user

--- src/pages/post/[postID]/index.jsx
+++ src/pages/post/[postID]/index.jsx
@@ -1,14 +1,14 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 
 export default function PostID() {
-  let { title, body } = useLoaderData();
+  let { title, body, userId } = useLoaderData();
   let params = useParams();
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-5xl">{title}</h1>
       <p className="pt-16 text-lg">{body}</p>
-      <p>This is /post/{params.postID}</p>
+      <Link to={`/user/${userId}`}>User {userId}</Link>
     </div>
   );
   //
