cf6c4bf23537fdb9db04b4b6fa612ac09193244e
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-09-07 10:24:57 +0200

    feat: :sparkles: add final tailwind config

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 export default function Index() {
   return (
     <>
-      <h1>hello, this is index</h1>
+      <h1 className="flex items-center text-5xl">hello, this is index</h1>
       <Link to="route">Link to route</Link>
     </>
   );
