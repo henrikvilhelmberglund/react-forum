76660f992076bceee4b3669bc024cb3c67647b96
LeoM <leobmagndahl@gmail.com>
2023-09-07 12:38:20 +0200

    home done

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,23 +1,33 @@
 import axios from "axios";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
 
 export default function Home() {
   const data = useLoaderData();
-  // console.log(data);
 
   return (
     <>
       <div>
-        <h2>This is /home</h2>
+        <h2 className="text-center">This is /home</h2>
       </div>
       <div className="flex justify-center">
         <div>
-          {data.map((post) => {
-            {
-              console.log(post.title);
-            }
-            <h2>Title: {post.title}</h2>;
-          })}
+          {data.map((post) => (
+            <div
+              className="border-2 border-black p-2 m-4 shadow-md shadow-gray-400"
+              key={post.id}
+            >
+              <h2>
+                {console.log(post)}
+                <span className="font-bold text-lg">Title: </span>
+                <Link
+                  className="text-black hover:text-blue-700 underline"
+                  to={`post/${post.id}`}
+                >
+                  {post.title}
+                </Link>
+              </h2>
+            </div>
+          ))}
         </div>
       </div>
     </>
