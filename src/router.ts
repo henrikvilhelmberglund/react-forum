7e8bbb2d036aa8cd31c63988749b111f05988c6a
Ingela Jonsson <ingela.ma.jonsson@gmail.com>
2023-09-07 13:11:05 +0200

    hej

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,7 @@ import { components, hooks, utils } from '@generouted/react-router/client'
 export type Path =
   | `/`
   | `/post/:postID`
+  | `/profile`
   | `/profile/:userID`
   | `/route`
   | `/route/:id`
